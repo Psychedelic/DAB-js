7373bab72e44cd81a21c5aef6f9b78b863e7b8bb
aleacevedo <ale.acevedo@live.com.ar>
2021-09-10 11:37:11 -0300

    feat: expose some methods to get standards

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,58 @@
 import { HttpAgent } from '@dfinity/agent';
-import NFT from './nft';
+import { Principal } from '@dfinity/principal';
+import NFT, { NFTDetails } from './nft';
 import EXT from './standards/ext';
+import ICPunks from './standards/icpunks';
+import { getAllNFTS } from './utils/dab';
 
-type NFTStandards = typeof EXT;
+const DAB_RESULT = [
+  { standard: 'ext', canisterId: 'some-id', name: 'entrepot' },
+];
+
+type NFTStandards = typeof EXT | typeof ICPunks;
 
 const NFT_STANDARDS: { [key: string]: NFTStandards } = {
   ext: EXT,
+  icpunks: ICPunks,
 };
 
-export const getNFT = (
+interface NFTRegistry {
+  name: string;
+  canisterId: string;
+  toknes: NFTDetails[];
+}
+
+interface GetAllUserNFTsResponse {
+  [standard: string]: NFTRegistry;
+}
+
+export const getNFTActor = (
   canisterId: string,
   agent: HttpAgent,
   standard: string
 ): NFT => {
   return new NFT_STANDARDS[standard](canisterId, agent);
+};
+
+export const getAllUserNFTs = async (
+  agent: HttpAgent,
+  user: Principal
+): Promise<GetAllUserNFTsResponse> => {
+  const NFTRegistry = await getAllNFTS(agent);
+  const result = {};
+  NFTRegistry.map((dab) => {
+    const NFTActor = getNFTActor(
+      dab.principal_id.toString(),
+      agent,
+      dab.standard
+    );
+    const details = NFTActor.getUserTokens(user);
+    if (!(dab.standard in result)) result[dab.standard] = [];
+    result[dab.standard] = {
+      name: dab.name,
+      canisterId: dab.principal_id.toString(),
+      tokens: details,
+    };
+  });
+  return result;
 };
