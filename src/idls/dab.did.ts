4338a1eac5bb1445cc5ea8988189cf49423af926
rocky-fleek <tomas@fleek.co>
2021-09-16 09:50:19 -0300

    Updated candid files

--- src/idls/dab.did.ts
+++ src/idls/dab.did.ts
@@ -1,17 +1,49 @@
 /* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable camelcase */
+
 export default ({ IDL }) => {
-  const getAllResult = IDL.Vec(
-    IDL.Record({
-      name: IDL.Text,
-      principal_id: IDL.Principal,
-      standard: IDL.Text,
-    })
-  );
+  const InputNFTCanister = IDL.Record({
+    'icon' : IDL.Text,
+    'name' : IDL.Text,
+    'description' : IDL.Text,
+    'principal_id' : IDL.Principal,
+    'standard' : IDL.Text,
+  });
+  const OperationError = IDL.Variant({
+    'NotAuthorized' : IDL.Null,
+    'BadParameters' : IDL.Null,
+    'NonExistentCanister' : IDL.Null,
+    'ParamatersNotPassed' : IDL.Null,
+  });
+  const OperationResponse = IDL.Variant({
+    'Ok' : IDL.Bool,
+    'Err' : OperationError,
+  });
+  const NFTCanister = IDL.Record({
+    'icon' : IDL.Text,
+    'name' : IDL.Text,
+    'description' : IDL.Text,
+    'timestamp' : IDL.Nat64,
+    'principal_id' : IDL.Principal,
+    'standard' : IDL.Text,
+  });
   return IDL.Service({
-    get_all: IDL.Func([], [getAllResult], []),
+    'add' : IDL.Func([InputNFTCanister], [OperationResponse], []),
+    'edit' : IDL.Func(
+        [
+          IDL.Text,
+          IDL.Opt(IDL.Principal),
+          IDL.Opt(IDL.Text),
+          IDL.Opt(IDL.Text),
+          IDL.Opt(IDL.Text),
+        ],
+        [OperationResponse],
+        [],
+      ),
+    'get_all' : IDL.Func([], [IDL.Vec(NFTCanister)], ['query']),
+    'get_canister' : IDL.Func([IDL.Text], [IDL.Opt(NFTCanister)], ['query']),
+    'name' : IDL.Func([], [IDL.Text], ['query']),
+    'remove' : IDL.Func([IDL.Text], [OperationResponse], []),
   });
 };
-export const init = () => {
-  return [];
-};
+export const init = ({ IDL }) => { return []; };
--- src/interfaces/dab.ts
+++ src/interfaces/dab.ts
@@ -2,16 +2,40 @@
 /* eslint-disable @typescript-eslint/class-name-casing */
 import { Principal } from '@dfinity/principal';
 
-interface DABCollection {
-  name: string;
-  principal_id: Principal;
-  standard: string;
-  icon?: string;
-  description?: string;
-}
-
 export type GetAllResult = DABCollection[];
 
+export interface DABCollection {
+  'icon' : string,
+  'name' : string,
+  'description' : string,
+  'principal_id' : Principal,
+  'standard' : string,
+};
+export interface NFTCanister {
+  'icon' : string,
+  'name' : string,
+  'description' : string,
+  'timestamp' : bigint,
+  'principal_id' : Principal,
+  'standard' : string,
+};
+export type OperationError = { 'NotAuthorized' : null } |
+  { 'BadParameters' : null } |
+  { 'NonExistentCanister' : null } |
+  { 'ParamatersNotPassed' : null };
+export type OperationResponse = { 'Ok' : boolean } |
+  { 'Err' : OperationError };
 export default interface _SERVICE {
-  get_all: () => Promise<GetAllResult>;
-}
+  'add' : (arg_0: DABCollection) => Promise<OperationResponse>,
+  'edit' : (
+      arg_0: string,
+      arg_1: [] | [Principal],
+      arg_2: [] | [string],
+      arg_3: [] | [string],
+      arg_4: [] | [string],
+    ) => Promise<OperationResponse>,
+  'get_all' : () => Promise<GetAllResult>,
+  'get_canister' : (arg_0: string) => Promise<[] | [NFTCanister]>,
+  'name' : () => Promise<string>,
+  'remove' : (arg_0: string) => Promise<OperationResponse>,
+};
