dc195742a5e83dbdfcc15a369adfe4906f41e6a4
aleacevedo <ale.acevedo@live.com.ar>
2021-09-28 15:11:06 -0300

    fix: use getNFTActor properly

--- src/dab_canisters/nfts_registry.ts
+++ src/dab_canisters/nfts_registry.ts
@@ -19,10 +19,10 @@ export const getNFTActor = (
   canisterId: string,
   agent: HttpAgent,
   standard: string
-): NFT | undefined => {
+): NFT => {
   if (!(standard in NFT_STANDARDS)) {
-    console.error('Standard is not implemented');
-    return undefined;
+    console.error(`Standard ${standard} is not implemented`);
+    throw new Error(`standard is not supported: ${standard}`);
   }
   return new NFT_STANDARDS[standard](canisterId, agent);
 };
@@ -42,12 +42,12 @@ export const getAllUserNFTs = async (
   const NFTCollections = await getAllNFTS(agent);
   const result = await Promise.all(
     NFTCollections.map(async (collection) => {
-      const NFTActor = getNFTActor(
-        collection.principal_id.toString(),
-        agent,
-        collection.standard
-      );
       try {
+        const NFTActor = getNFTActor(
+          collection.principal_id.toString(),
+          agent,
+          collection.standard
+        );
         const details = await NFTActor.getUserTokens(user);
         return {
           name: collection.name,
