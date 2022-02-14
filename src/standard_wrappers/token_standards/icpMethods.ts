61904696e5e0b125a6b3aba6dd7c08365e90148f
aleacevedo <ale.acevedo@live.com.ar>
2022-02-14 17:28:27 -0300

    fix: use account id to send ICP

--- src/standard_wrappers/token_standards/icpMethods.ts
+++ src/standard_wrappers/token_standards/icpMethods.ts
@@ -18,7 +18,7 @@ import { getAccountId } from '../../utils/account';
 
 type BaseLedgerService = BaseMethodsExtendedActor<LedgerService>;
 
-const DECIMALS = 8
+const DECIMALS = 8;
 
 const NET_ID = {
   blockchain: 'Internet Computer',
@@ -33,7 +33,7 @@ const getMetadata = async (
     fungible: {
       symbol: 'ICP',
       decimals: DECIMALS,
-      name: 'ICP'
+      name: 'ICP',
     },
   };
 };
@@ -42,20 +42,20 @@ const send = async (
   actor: ActorSubclass<BaseLedgerService>,
   { to, amount, opts }: SendParams
 ): Promise<SendResponse> => {
-    const defaultArgs = {
-      fee: BigInt(10000),
-      memo: BigInt(0),
-    };
-    const response = await actor._send_dfx({
-      to,
-      fee: { e8s: opts?.fee || defaultArgs.fee },
-      amount: { e8s: amount },
-      memo: opts?.memo ? BigInt(opts.memo) : defaultArgs.memo,
-      from_subaccount: [], // For now, using default subaccount to handle ICP
-      created_at_time: [],
-    });
+  const defaultArgs = {
+    fee: BigInt(10000),
+    memo: BigInt(0),
+  };
+  const response = await actor._send_dfx({
+    to: getAccountId(Principal.fromText(to)),
+    fee: { e8s: opts?.fee || defaultArgs.fee },
+    amount: { e8s: amount },
+    memo: opts?.memo ? BigInt(opts.memo) : defaultArgs.memo,
+    from_subaccount: [], // For now, using default subaccount to handle ICP
+    created_at_time: [],
+  });
 
-    return { height: await response.toString() }
+  return { height: await response.toString() };
 };
 
 const getBalance = async (
@@ -92,13 +92,13 @@ const burnXTC = async (
   throw new Error('BURN NOT SUPPORTED');
 };
 
-const getDecimals = async (actor: ActorSubclass<BaseLedgerService>) => getDecimalsFromMetadata(await getMetadata(actor))
-
+const getDecimals = async (actor: ActorSubclass<BaseLedgerService>) =>
+  getDecimalsFromMetadata(await getMetadata(actor));
 
 export default {
   send,
   getMetadata,
   getBalance,
   burnXTC,
-  getDecimals
+  getDecimals,
 } as InternalTokenMethods;
