e5ec0ae7948e2a15b76cc57f16720883ee5055fd
rocky-fleek <tomas@fleek.co>
2022-07-19 11:08:38 +0200

    Removed tokenID + principal split in getUserTokens

--- src/standard_wrappers/nft_standards/nft_origyn.ts
+++ src/standard_wrappers/nft_standards/nft_origyn.ts
@@ -30,10 +30,8 @@ export default class NFTOrigyn extends NFT<string, string> {
     }
     const tokensData = await Promise.all(
       tokensIndexes.ok.nfts.map(async (item) => {
-        const tokenIndex = item[0]
-        const principal = item[1]
-        const userTokensResult = await this.actor.nft_origyn(tokenIndex);
-        if ('err' in userTokensResult)
+        const userTokensResult = await this.actor.nft_origyn(item);
+        if ('err' in userTokensResult) 
           throw new Error(Object.keys(userTokensResult.err)[0]);
         return {detail: userTokensResult, principal};
       })
