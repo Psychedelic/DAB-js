58ca2c7dc777de5406e5b5e708440f8cc7c4c405
rocky-fleek <tomas@fleek.co>
2022-11-11 09:33:53 -0600

    Removed renames

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ export *  from './interfaces/token';
 export * as NFTInterfaces from './interfaces/nft';
 export * as TokenInterfaces from './interfaces/token'
 export { default as standards } from './constants/standards';
-export { Principal as LegacyPrincipal } from '@dfinity/principal';
-export { HttpAgent as LegacyHttpAgent } from '@dfinity/agent';
+export { Principal } from '@dfinity/principal';
+export { HttpAgent } from '@dfinity/agent';
