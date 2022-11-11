f73882fa0daebb137bc0a1780e575316c3f9b46e
rocky-fleek <tomas@fleek.co>
2022-11-11 09:32:40 -0600

    Added exports for 0.9.3 agent and principal

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ export *  from './interfaces/token';
 export * as NFTInterfaces from './interfaces/nft';
 export * as TokenInterfaces from './interfaces/token'
 export { default as standards } from './constants/standards';
+export { Principal as LegacyPrincipal } from '@dfinity/principal';
+export { HttpAgent as LegacyHttpAgent } from '@dfinity/agent';
